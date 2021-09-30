e52afa16604454977bd7ad4b7ab4e894aae01d34
mateen kazia <mateen.kazia@chaione.com>
2021-09-30 13:22:53 -0500

    Spike: Order CMS Content by Drag and Drop
    Created HOC component for DnD that reusable for different drag and drop needs
    Created collapsable reusable row for edit section
    
    #FXCMS-403

--- components/CollapseRow/CollapseRow.jsx
+++ components/CollapseRow/CollapseRow.jsx
@@ -1,31 +1,30 @@
-import React, { useState } from "react";
-import Image from "next/image";
+import React, { useState } from 'react';
+import Image from 'next/image';
 // MUI
-import Box from "@mui/material/Box";
-import Collapse from "@mui/material/Collapse";
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
 import {
   Container,
   Text,
   Main,
   DropDown,
   StyledButton,
-  DropDownText,
-} from "./CollapseRow.styles";
+  DropDownText
+} from './CollapseRow.styles';
 // Icons
-import Up from "../../images/up.svg";
-import Down from "../../images/down.svg";
-import Plus from "../../images/Plus.svg";
-import { Draggable } from "react-beautiful-dnd";
-import { DragDropCollapseRow, DragDropContainer} from "@components";
-
+import Up from '../../images/up.svg';
+import Down from '../../images/down.svg';
+import Plus from '../../images/plus.svg';
+import { Draggable } from 'react-beautiful-dnd';
+import { DragDropCollapseRow, DragDropContainer } from '@components';
 
 const detailList = [
-  { id: 1, name: "George Clooney" },
-  { id: 2, name: "Steve Carell" },
-  { id: 3, name: "Barbra Streisand" },
-  { id: 4, name: "James Patterson" },
-  { id: 5, name: "Ben Stiller" },
-  { id: 6, name: "Jeff Gordon" },
+  { id: 1, name: 'George Clooney' },
+  { id: 2, name: 'Steve Carell' },
+  { id: 3, name: 'Barbra Streisand' },
+  { id: 4, name: 'James Patterson' },
+  { id: 5, name: 'Ben Stiller' },
+  { id: 6, name: 'Jeff Gordon' }
 ];
 
 const CollapseRow = ({ name }) => {
@@ -36,31 +35,31 @@ const CollapseRow = ({ name }) => {
     <Container>
       <Main onClick={() => setOpen(!open)}>
         <Text>{name}</Text>
-        <Box p="20px 0px">
-          {open ? <Image src={Up} alt="Up" /> : <Image src={Down} alt="down" />}
+        <Box p='20px 0px'>
+          {open ? <Image src={Up} alt='Up' /> : <Image src={Down} alt='down' />}
         </Box>
       </Main>
-      {name === "Quotes" ? (
+      {name === 'Quotes' ? (
         <Collapse in={open} timeout={500}>
           <DropDown>
             <Box
-              display="flex"
-              justifyContent="space-between"
-              alignContent="center"
-              p="8px 0"
+              display='flex'
+              justifyContent='space-between'
+              alignContent='center'
+              p='8px 0'
             >
               <DropDownText>{name.toUpperCase()}</DropDownText>
-              <StyledButton variant="text">
-                ADD QUOTE{" "}
-                <Box pl="20px">
-                  <Image src={Plus} alt="plus" />
+              <StyledButton variant='text'>
+                ADD QUOTE{' '}
+                <Box pl='20px'>
+                  <Image src={Plus} alt='plus' />
                 </Box>
               </StyledButton>
             </Box>
             <DragDropContainer
               items={items}
               setItems={setItems}
-              droppableId="quotes"
+              droppableId='quotes'
             >
               {items.map((item, idx) => (
                 <Draggable
